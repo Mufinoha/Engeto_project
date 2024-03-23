b8b8df6b4560fceeacb65d7dce641bf102511ad5
Martin Lebl <lebl.martin@atlas.cz>
2024-03-23 11:55:05 +0100

    Added Question number 1

---
+++ Question number 1 Lebl_Martin.sql
@@ -0,0 +1,28 @@
+SELECT *
+FROM t_martin_lebl_project_sql_primary_final_1 pf 
+
+WITH avg_wages AS (
+	SELECT 
+		`year`,
+		name,
+		round(avg(value)) AS avg_wages
+	FROM t_martin_lebl_project_sql_primary_final_1 AS pf 
+	GROUP BY `year`,name
+	)
+SELECT
+	DISTINCT aw.name,
+	aw.`year`,
+	aw.avg_wages,
+	aw1.`year`,
+	aw1.avg_wages AS previous_year_avg_wages,
+	CASE 
+		WHEN (aw.avg_wages-aw1.avg_wages) > 1 THEN lower('ASCENDING')
+		ELSE 'DESCENDING'
+	END AS wage_comparison
+FROM avg_wages AS aw
+JOIN avg_wages AS aw1
+	ON aw.name = aw1.name AND 
+	aw.`year` = aw1.`year`+1
+ORDER BY
+	aw.name ASC,
+	aw.`year` DESC;
